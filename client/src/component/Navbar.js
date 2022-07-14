4a3eb375c4ec25c469a4f5a36aa1cc95b62de03c
Neha Jha <nehajha8720@gmail.com>
2022-07-14 21:41:52 +0530

    first commit

---
+++ client/src/component/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+                <div className="container-fluid">
+                    <NavLink className="navbar-brand text-white" to="/">CRUD APP</NavLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active text-white" aria-current="page" href="/">Home</a>
+                            </li>
+                          
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
